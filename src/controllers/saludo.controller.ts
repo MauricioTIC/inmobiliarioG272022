29bf0afd7e62f5e2e4a16d9e9db6f0ef5c2064d6
maocber <maocber@users.noreply.github.com>
2022-10-12 19:55:05 -0500

    Versión Inicial  de Inmobiliria 27

---
+++ src/controllers/saludo.controller.ts
@@ -0,0 +1,15 @@
+// Uncomment these imports to begin using these cool features!
+
+import {get} from '@loopback/rest';
+
+
+
+export class SaludoController {
+  constructor() {}
+
+  //router.get('/saludar)
+  @get('/saludar')
+  saludar(): string {
+    return 'Hola desde mi 1er controlador en Loopback';
+  }
+}
